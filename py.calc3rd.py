4bd92bacaf85dd938b3a417b226defb7bc2da9ca
usersaniya <165312391+usersaniya@users.noreply.github.com>
2025-03-16 20:29:32 +0530

    Add files via upload
    this program in python simply takes two integer inputs and performs very basic mathematical operations on them one by one.

---
+++ py.calc3rd.py
@@ -0,0 +1,12 @@
+#calculator
+a=int(input("Enter 1st number:"))
+b=int(input("Entr 2nd number:"))
+print("addition", a, "+", b, "=" , a+b)
+print("subtraction", a, "-", "=" , b, a-b)
+print("multiplication", a, "*", "=" , b ,a*b)
+print("division", a, "/", b, "=" , a/b)
+print("floor division",  a, "//", b , "=" , a//b)
+print("exponent",  a, "**", b , "=" , a**b)
+print("modulo", a, "%", b,  "=" , a%b)
+
+
